327040923bcdba505a0af5377b31ca9877504f9f
ShubhamPnd <60984701+ShubhamPnd@users.noreply.github.com>
2020-09-18 02:33:05 +0530

    Added Patterns Problems

---
+++ Patterns/Pattern_2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Pattern 2");
+		
+		Scanner scn = new Scanner(System.in);
+		int number = scn.nextInt();
+		
+		int temp = number;
+		
+		for(int i = 1 ;i <= number;i++){
+		     for (int j = 1;j <= temp ;j++){
+		          System.out.print("*\t");
+		     } 
+		     temp--;
+		     System.out.println();
+		}
+	}
+}
+
